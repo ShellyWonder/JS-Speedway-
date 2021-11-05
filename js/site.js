c2770c57462e159439503e635ac37a0a90ba0600
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2021-11-04 23:09:15 -0500

    added js varibles & debugging fuctionality

--- js/site.js
+++ js/site.js
@@ -1,4 +1,17 @@
-//placeholder code--call Hello World
-function HelloWorld() {
-  alert("Hello World");
+//get the values from inputs
+//starts (controller function)
+function getValues() {
+  //get values from inputs
+  //call generateNumbers
+  //call  displayNumbers
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
+  alert("the start value:" + startValue);
 }
+
+//generate numbers from startValue to the endValue
+//logic function(s)
+function generateNumber() {}
+//display numbers and mark even ones "Bold"
+//display or view functions (RETURN)
+function displayNumbers() {}
