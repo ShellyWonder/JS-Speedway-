787a20e9a86ef511ec73ffeedbcaaf35c310fd98
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2021-11-05 18:33:55 -0500

    declaring varibles, declaring functions & for loop

--- js/site.js
+++ js/site.js
@@ -2,16 +2,24 @@
 //starts (controller function)
 function getValues() {
   //get values from inputs
-  //call generateNumbers
-  //call  displayNumbers
+
   let startValue = document.getElementById("startValue").value;
   let endValue = document.getElementById("endValue").value;
   alert("the start value:" + startValue);
+
+  //call generateNumbers
+  let numbers = generateNumbers();
+
+  //call  displayNumbers
 }
 
 //generate numbers from startValue to the endValue
 //logic function(s)
-function generateNumber() {}
+function generateNumbers(sValue, eValue) {
+  let numbers = [];
+  for (let index = sValue; index <= eValue; index++) {}
+  return numbers;
+}
 //display numbers and mark even ones "Bold"
 //display or view functions (RETURN)
 function displayNumbers() {}
