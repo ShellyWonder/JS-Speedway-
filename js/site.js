6c99d5e2c072c29f7b450e717025e64cb4e19cd6
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2021-11-06 09:58:25 -0500

    finished js & populated prism section in code.html

--- js/site.js
+++ js/site.js
@@ -2,24 +2,48 @@
 //starts (controller function)
 function getValues() {
   //get values from inputs
-
   let startValue = document.getElementById("startValue").value;
   let endValue = document.getElementById("endValue").value;
-  alert("the start value:" + startValue);
 
-  //call generateNumbers
-  let numbers = generateNumbers();
+  //VALIDATE INPUTS to convert strings to integers
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
 
-  //call  displayNumbers
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    //call generateNumbers():
+    let numbers = generateNumbers(startValue, endValue);
+    //call displayNumbers():
+    displayNumbers(numbers);
+  } else {
+    alert("You must enter numbers.");
+  }
 }
 
 //generate numbers from startValue to the endValue
 //logic function(s)
 function generateNumbers(sValue, eValue) {
   let numbers = [];
-  for (let index = sValue; index <= eValue; index++) {}
+  for (let index = sValue; index <= eValue; index++) {
+    numbers.push(index);
+  }
   return numbers;
 }
-//display numbers and mark even ones "Bold"
+
 //display or view functions (RETURN)
-function displayNumbers() {}
+function displayNumbers(numbers) {
+  let templateRows = "";
+  for (let index = 0; index < numbers.length; index++) {
+    //mark even numbers "Bold"
+    let className = "even";
+    let number = numbers[index];
+    ///Determining even numbers for Bold feature
+    if (number % 2 == 0) {
+      className = "even";
+    } else {
+      className = "odd";
+    }
+
+    templateRows += `<tr><td class="${className}">${number}</tr></tr>`;
+  }
+  document.getElementById("results").innerHTML = templateRows;
+}
